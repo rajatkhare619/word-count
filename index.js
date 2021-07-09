1e7168a09027e97e26f73ff868e93c37a4c5c8af
Rajat Khare <rajat.khare@pb.com>
2021-07-09 17:08:24 +0530

    Count the number of words in a sentence

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello, world');
+const utils = require('./utils');
+
+
+utils.readLine('Enter text ').then(res => {
+    let regEx = /([a-zA-Z])+/g;
+    let match = res.match(regEx);
+    if (match) {
+        console.log(`Number of words: ${match.length}`);
+    } else {
+        console.log(`Number of words: 0`);
+    }
+});
+
+
+
